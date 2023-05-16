114bf653c7621fc0f2e7748aafcfc3dc6a685dab
harshit070607 <harshitsoni0706@gamil.com>
2023-05-16 14:06:57 +0530

    commit2

--- project1.js
+++ project1.js
@@ -1,20 +1,26 @@
 
 var form1 = document.getElementById('formName');
+var ke = 0;
 
 form1.addEventListener('submit', addInfo);
 
 function addInfo(e){
     e.preventDefault();
+    ke=ke+1;
 
-var Name = document.getElementById('name').value;
-var Email = document.getElementById('email').value;
+    var Name = document.getElementById('name').value;
+    var Email = document.getElementById('email').value;
+    
+    var obj = {
 
-if(localStorage.getItem(Name,Email))
-{
-    alert('Already exist');
-}
-else
-{
-localStorage.setItem(Name,Email);
-}
+        name : Name ,
+        email : Email
+
+    }
+
+
+    let obj_serialized = JSON.stringify(obj);
+
+localStorage.setItem(ke,obj_serialized);
+ 
 }
