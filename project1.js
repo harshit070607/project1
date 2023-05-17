2600fe47ff5a6bb8443acb9041031dbe9ebeb2e6
harshit070607 <harshitsoni0706@gamil.com>
2023-05-17 22:42:25 +0530

    commit4

--- project1.js
+++ project1.js
@@ -7,20 +7,18 @@ var showItems = document.getElementById('detail');
 
 form1.addEventListener('submit', addInfo);
 
+var dlt = document.getElementById('detail');
+
+dlt.addEventListener('click',deleteitem);
+
+
+
 function addInfo(e){
     e.preventDefault();
     
     var Name = document.getElementById('name').value;
     var Email = document.getElementById('email').value;
 
-   
-    var li2 = document.createElement('li');
-    li2.className="items";
-    li2.appendChild(document.createTextNode(Email));
-    li2.append(" is the email id of "+Name);
-
-    showItems.appendChild(li2);
-    
     var obj = {
 
         name : Name ,
@@ -28,17 +26,49 @@ function addInfo(e){
 
     }
 
+
 if(localStorage.getItem(obj.email))
 {
     alert('Aready exist');
 }
 else
 {
-   
 
+    var li2 = document.createElement('li');
+    li2.className="items";
+    li2.appendChild(document.createTextNode(Email));
+    li2.append(" - "+Name);
+    showItems.appendChild(li2);
+
+    var dltbtn = document.createElement('button');
+    dltbtn.className='btn btn-danger btn-sm mx-2 my-2';
+    var texbtn = document.createTextNode("Delete");
+    dltbtn.appendChild(texbtn);
+    li2.appendChild(dltbtn);
+
+    
+    dltbtn.onclick=()=>
+    {
+        localStorage.removeItem(obj.email);
+        var rem = e.target.parentElement;
+        detail.removeChild(rem);
+        
+    }
+
+
+   
 //add data to local storage
 let obj_serialized = JSON.stringify(obj);
 localStorage.setItem(obj.email,obj_serialized);
 }
  
+}
+
+function deleteitem(e){
+
+    
+
+   // delete from UI
+    var rem = e.target.parentElement;
+    detail.removeChild(rem);
 }
